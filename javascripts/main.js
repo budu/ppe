d735903b9c37e0922b297a3146aa792355fba031
Nicolas Buduroi <nbuduroi@gmail.com>
2012-09-08 14:24:39 -0400

    Updated website.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,12 @@
 (function() {
 
   $(function() {
-    return $('a.show-more').click(function(e) {
-      $(this).slideUp().siblings('.more').hide().removeClass('hidden').slideDown(1000);
+    $('a.show-more').click(function(e) {
+      $(this).slideUp().siblings('.more').hide().removeClass('hidden').slideDown(1000).siblings('.show-less').slideDown();
+      return e.preventDefault();
+    });
+    return $('a.show-less').click(function(e) {
+      $(this).slideUp().siblings('.more').slideUp(1000).siblings('.show-more').slideDown();
       return e.preventDefault();
     });
   });
